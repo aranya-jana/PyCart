5b4df84d0d6259bc5f193bc808f2960324de8035
Aranya Jana <aranyajana01@gmail.com>
2025-06-27 20:37:02 +0530

    Update product section

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from category.models import Category
+
 # Create your models here.
+
+class Product(models.Model):
+    product_name    = models.CharField(max_length=200, unique= True)
+    slug            = models.SlugField(max_length= 200, unique= True)
+    description     = models.TextField(max_length= 500, blank=True)
+    price           = models.IntegerField()
+    image           = models.ImageField(upload_to='photos/products')
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default= True)
+    cetegory        = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date    = models.DateTimeField(auto_now_add=True)
+    modified_field  = models.DateTimeField(auto_add=True)
