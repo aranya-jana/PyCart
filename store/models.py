0b68b8a66c9b348e7362f3153d93172785b1f840
Aranya Jana <aranyajana01@gmail.com>
2025-07-02 00:14:30 +0530

    Add get_url for Product

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from category.models import Category
 
@@ -20,5 +21,10 @@ class Product(models.Model):
 
 
 #string representation 
-def __str__(self):
-    return self.product_name
+
+    def get_url(self):
+        return reverse('product_detail', args = [self.category.slug, self.slug])
+
+
+    def __str__(self):
+        return self.product_name
